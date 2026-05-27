3c5aa868c7ec15c5402398cf90f8e64ac6910e22
Maranhao <joaofilipefonseca.alu.ufc.br>
2026-05-27 18:06:11 -0300

    casais feito e agua caiu no meu notebook

--- tasks/ed/casais/src/go/draft.go
+++ tasks/ed/casais/src/go/draft.go
@@ -1,19 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
 func main() {
 
-    var n int //numero de animais
-    fmt.Scan(&n) //
-    //vamo trabalhar com vetores
-    var s[]int //s de solteiros
-    var pares int
+	var n int    //numero de animais
+	fmt.Scan(&n) //
+	//vamo trabalhar com vetores
+	s := []int{} //s de solteiros
+	var pares int = 0
 
-    for i:=0;i<n;i++{
-        var animal int
-        fmt.Scan(&animal)
-        if animal
-    }
+	for i := 0; i < n; i++ {
+		var animal int
+		fmt.Scan(&animal)
 
+		casados := false
+		for j := 0; j < len(s); j++ {
+			if s[j] == (animal * (-1)) {
+				s[j] = 0
+				casados = true
+				pares++
+				break
+			}
+		}
+		if !casados {
+			s = append(s, animal)
+		}
+	}
 
+	fmt.Printf("%d\n", pares)
 }
